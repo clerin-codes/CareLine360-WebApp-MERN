fc8513957386400f6b18cc3bab50438fc6ce245f
clerin-codes <sheronclerin1805@gmail.com>
2026-02-12 00:10:35 +0530

    configure backend folder structures

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-colors">
           count is {count}
         </button>
         <p>
